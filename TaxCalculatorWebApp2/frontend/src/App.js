ca7ec42abe8cb07d2143c466de809fe2c689962b
krishdabole <130666346+KrishDabole@users.noreply.github.com>
2025-11-20 06:30:04 +0530

    Delete TaxCalculatorWebApp2 directory

--- TaxCalculatorWebApp2/frontend/src/App.js
+++
@@ -1,4 +0,0 @@
-import React, { useState } from 'react';
-import Calculator from './Calculator';
-function App(){ const [token,setToken]=useState('dummy'); return (<div style={{padding:20}}>{token? <Calculator token={token}/> : <div>Login</div>}</div>);}
-export default App;
