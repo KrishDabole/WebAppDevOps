bfbdcc73520550ff4b62a644501ecf885fa41948
krishdabole <130666346+KrishDabole@users.noreply.github.com>
2025-11-20 06:30:16 +0530

    Delete TaxCalculatorWebApp3 directory

--- TaxCalculatorWebApp3/backend/src/main/java/com/example/taxcalculator/repo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.taxcalculator.repo;
-import com.example.taxcalculator.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByEmail(String email);
-}
